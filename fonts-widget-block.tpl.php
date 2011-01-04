1817a1572245d4cb2fbb12cc717f4279a9e8475b
Khaled Alhourani <me@khaledalhourani.com>
2011-01-04 13:12:55 +0200

    replacing old html code with API template

--- fonts-widget-block.tpl.php
+++ fonts-widget-block.tpl.php
@@ -7,43 +7,23 @@
  *
  * Available variables:
  * - $user: User profile.
+ * - $items: widget items.
  *
  * @see template_preprocess_fonts_widget_block()
  */
 ?>
 
-<div class="fonts-widget-container" id="face">
-  <div class="fonts-widget-title">الخط:</div>
-  <div class="fonts-widget-button">
-    <div class="widget-item typeface" id="uthmani" title="الخط العثماني"></div>
-  </div>
-  <div class="fonts-widget-button">
-    <div class="widget-item typeface" id="arabic" title="الخط العربي التقليدي"></div>
-  </div>
-</div>
+<?php foreach ($items as $item): ?>
+  <div class="fonts-widget-container" id="<?php print $item['name']; ?>">
+    <div class="fonts-widget-title"><?php print $item['title']; ?></div>
 
-<div class="fonts-widget-container" id="size">
-  <div class="fonts-widget-title">الحجم:</div>
-  <div class="fonts-widget-button">
-    <div class="widget-item size" id="size-1" title="صغير"></div>
-  </div>
-  <div class="fonts-widget-button">
-    <div class="widget-item size" id="size-2" title="متوسط"></div>
-  </div>
-  <div class="fonts-widget-button">
-    <div class="widget-item size" id="size-3" title="كبير"></div>
-  </div>
-</div>
+    <?php foreach ($item as $subitem): ?>
+      <?php if (is_array($subitem)): ?>
+      <div class="fonts-widget-button">
+        <div class="widget-item <?php print $item['class']; ?>" id="<?php print $subitem['name']; ?>" title="<?php print $subitem['title']; ?>"></div>
+      </div>
+      <?php endif; ?>
+    <?php endforeach; ?>
 
-<div class="fonts-widget-container" id="height">
-  <div class="fonts-widget-title">عدد اﻷسطر:</div>
-  <div class="fonts-widget-button">
-    <div class="widget-item height" id="height-1" title="قليل"></div>
   </div>
-  <div class="fonts-widget-button">
-    <div class="widget-item height" id="height-2" title="عادي"></div>
-  </div>
-  <div class="fonts-widget-button">
-    <div class="widget-item height" id="height-3" title="كثير"></div>
-  </div>
-</div>
+<?php endforeach; ?>
